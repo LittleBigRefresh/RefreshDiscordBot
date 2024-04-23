705a93b3b664275dc657086e8fdc2d821814d1b3
jvyden <jvyden@jvyden.xyz>
2024-04-23 04:17:27 -0400

    add stanky maymays

--- RefreshDiscordBot/Modules/RandomActivityModule.cs
+++ RefreshDiscordBot/Modules/RandomActivityModule.cs
@@ -7,7 +7,7 @@ namespace RefreshDiscordBot.Modules;
 // port from moniku
 public class RandomActivityModule(Bot bot, Logger logger) : Module(bot, logger)
 {
-    protected override uint RunFrequency => 60_000;
+    protected override uint RunFrequency => 120_000;
     private List<Activity> _activities = null!;
 
     public override Task Ready()
@@ -16,7 +16,26 @@ public class RandomActivityModule(Bot bot, Logger logger) : Module(bot, logger)
         [
             new StatisticActivity(ActivityType.Watching, stats => $"{stats.ActiveUsers} players"),
             new StatisticActivity(ActivityType.Playing, stats => $"{stats.TotalLevels} levels"),
-            new StatisticActivity(ActivityType.Playing, stats => $"with {stats.CurrentIngamePlayersCount} online players")
+            new StatisticActivity(ActivityType.Playing, stats => $"with {stats.CurrentIngamePlayersCount} online players"),
+            new StatisticActivity(ActivityType.CustomStatus, stats => $"{stats.TotalEvents} things have happened on Refresh"),
+            new Activity(ActivityType.CustomStatus, "https://littlebigrefresh.com"),
+            new Activity(ActivityType.CustomStatus, "Getting 100CR for a mod giveaway"),
+            new Activity(ActivityType.Playing, "Clockworx 2 for the 1000th time"),
+            new Activity(ActivityType.CustomStatus, "Sackboy gaming"),
+            new Activity(ActivityType.Watching, "the 64"),
+            new Activity(ActivityType.Watching, "big tc"),
+            new Activity(ActivityType.Playing, "LittleBigPlanet 2"),
+            new Activity(ActivityType.Playing, "LittleBigPlanet 4"), // lmao
+            new Activity(ActivityType.Playing, "LittleBigPlanet PSP"),
+            new Activity(ActivityType.Playing, "Toolkit"),
+            new Activity(ActivityType.CustomStatus, "True..."),
+            new Activity(ActivityType.CustomStatus, "False..."),
+            new Activity(ActivityType.Playing, "GOOD horror levels"),
+            new Activity(ActivityType.Playing, "terrible horror levels"),
+            new Activity(ActivityType.CustomStatus, "Surely these statuses won't get boring or unfunny"),
+            new Activity(ActivityType.CustomStatus, "fixed in the rewrite"),
+            new Activity(ActivityType.CustomStatus, "#ful-memes revival arc when"),
+            new Activity(ActivityType.Watching, "YOU, specifically"),
         ];
         
         return Task.CompletedTask;
@@ -27,8 +46,13 @@ public class RandomActivityModule(Bot bot, Logger logger) : Module(bot, logger)
         Activity activity = this._activities[Random.Shared.Next(0, this._activities.Count)];
         await activity.SetActivity(this.Bot, ct);
         
+        ct.ThrowIfCancellationRequested();
+        
         this.Log(LogLevel.Info, $"Our activity is now '*{activity.Type}* {activity.Text}'");
-        await this.Bot.Client.SetActivityAsync(new Game(activity.Text, activity.Type));
+        if (activity.Type == ActivityType.CustomStatus) 
+            await this.Bot.Client.SetCustomStatusAsync(activity.Text);
+        else
+            await this.Bot.Client.SetActivityAsync(new Game(activity.Text, activity.Type));
     }
 
     private class StatisticActivity : Activity
